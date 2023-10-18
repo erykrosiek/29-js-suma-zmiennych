1d09399bef806d4ed3657138a45e38acd1e532b3
erykrosiek <erykrosiekcollab@gmail.com>
2023-10-18 20:19:00 +0200

    29-js-suma-zmiennych

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber = 2;
+
+let secondNumber = 3;
+
+function dodawanie(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+let result = dodawanie(firstNumber, secondNumber);
+
+console.log(result);
